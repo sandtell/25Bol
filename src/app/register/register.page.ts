40f01f289aee9d5d82115791adb32d696e894185
sandtell <sandtellcustomercare@gmail.com>
2019-12-26 18:29:09 +0530

    registration validation

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NavController, LoadingController, ToastController, AlertController } from "@ionic/angular";
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Storage } from '@ionic/storage';
+import { Router } from '@angular/router';
+import { ConfigService } from '../services/config.service';
+import { HttpClient } from '@angular/common/http';
+import { PasswordValidator } from '../password.validator';
+
 
 @Component({
   selector: 'app-register',
@@ -6,10 +14,102 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-
-  constructor() { }
+  validations_form: FormGroup;
+  matching_passwords_group: FormGroup;
+  public showPassword: boolean = false;
+  public showConfirmPassword: boolean = false;
+  constructor(
+    private navCtrl: NavController,
+    public formBuilder: FormBuilder,
+    private storage: Storage,
+    public loadingCtrl: LoadingController,
+    public toastController: ToastController,
+    public alertCtrl: AlertController,
+    private router: Router,
+    private config: ConfigService,
+    public http: HttpClient,
+  ) { }
 
   ngOnInit() {
+
+    this.matching_passwords_group = new FormGroup({
+      password: new FormControl('', Validators.compose([
+        Validators.minLength(5),
+        Validators.required,
+        // Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')
+      ])),
+      confirm_password: new FormControl('', Validators.required)
+    }, (formGroup: FormGroup) => {
+      return PasswordValidator.areEqual(formGroup);
+    });
+
+    this.validations_form = this.formBuilder.group({
+      fullName: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z]+$')
+      ])),
+
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      mobileNo: new FormControl('', Validators.compose([
+        Validators.minLength(10),
+        Validators.maxLength(10),
+        Validators.required,
+        Validators.pattern('^[0-9]*$')
+      ])),
+      matching_passwords: this.matching_passwords_group,
+      oldpassword: new FormControl('', Validators.compose([
+        Validators.required,
+      ])),
+
+    });
   }
+
+  validation_messages = {
+    fullName: [
+      { type: 'required', message: 'Name is required.' },
+      { type: 'pattern', message: 'Number are not allowed' }
+    ],
+    email: [
+      { type: 'required', message: 'Email is required.' },
+      { type: 'pattern', message: 'Please enter a valid email.' }
+    ],
+    mobileNo: [
+      { type: 'required', message: 'Mobile number is required.' },
+      { type: 'minlength', message: 'Mobile No must be at least 10' },
+      { type: 'maxlength', message: 'Mobile No cannot be more than 10' },
+      { type: 'pattern', message: 'Chapter are not allowed' }
+    ],
+    oldpassword: [
+      { type: 'required', message: 'Old Password is required.' },
+    ],
+    password: [
+      { type: 'required', message: 'Password is required.' },
+      { type: 'minlength', message: 'Password must be at least 5 characters long.' },
+      // { type: 'pattern', message: 'Your password must contain at least one uppercase, one lowercase, and one number.' }
+    ],
+    confirm_password: [
+      { type: 'required', message: 'Confirm password is required.' }
+    ],
+    matching_passwords: [
+      { type: 'areEqual', message: 'Password mismatch.' }
+    ]
+  };
+
+  async onSubmit(values){
+    console.log(values);
+  }
+
+  onPasswordToggle(): void {
+    this.showPassword = !this.showPassword;
+  }
+
+  onConfirmPasswordToggle(): void {
+    this.showConfirmPassword = !this.showConfirmPassword;
+  }
+
+  
 
 }
