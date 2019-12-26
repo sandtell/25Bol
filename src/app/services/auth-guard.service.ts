bc248f66c40a15a59b407149c2f7656728466c55
sandtell <sandtellcustomercare@gmail.com>
2019-12-26 12:45:32 +0530

    first commit

---
+++ src/app/services/auth-guard.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { AuthenticationService } from './authentication.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardService {
+
+  constructor(
+    public authenticationService: AuthenticationService
+    ) { }
+
+    canActivate(): boolean {
+      return this.authenticationService.isAuthenticated();
+    }
+}
